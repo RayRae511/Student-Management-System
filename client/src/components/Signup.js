31422aeaace53c173e26947929dc98cd65f9a274
Raymond Oeta <oetaray@gmail.com>
2023-10-04 09:40:24 +0300

    added login, signin and admin files

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,83 +1,92 @@
-import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
+function SignUp() {
 
-const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [signUp, setSignUp] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
+  useEffect(() => {
+    const savedUsername = localStorage.getItem("username");
+    const savedPassword = localStorage.getItem("password");
+    if (savedUsername){
+      setUsername(savedUsername)
+    }
+    if (savedPassword){
+      setPassword(savedPassword)
+    }
+  }, [])
 
-  const handleChange = (e) => {
-    const {name, value} = e.target;
-    setSignUp({...signUp, [name]: value});
+  const handleInputChange = (e) => {
+    const {name, value } = e.target;
+    if (name === 'username'){
+      setUsername(value)
+    }
+    if (name === 'password'){
+      setPassword(value)
+    }
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("Form dubmitted with data:", signUp)
-  }
+    e.preventDefault();
+    localStorage.setItem("username", username)
+    localStorage.setItem("password", password)
+  };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="p-8 rounded shadow-md w-96">
-        <h1 className="text-2xl font-semibold mb-4 text-blue-400">Scholar</h1><h1 className="text-2xl font-semibold mb-4 text-blue-400">Student MS</h1>
-        <h2>Sign up</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-black text-sm font-bold mb-2">
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-blue-400/60 ring ring-2 ring-black lg-max-w-xl text-black">
+        <h1 className="text-3xl font-semibold text-center text-black uppercase">Sign up</h1>
+        <form className="mt-6" onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label
+            htmlFor="username"
+              className="block text-sm font-semibold text-black">
               Username
-            </label>
-            <input
-            type="text"
-            name="username"
-            value={signUp.username}
-            onChange={handleChange}
-            className="border border-blue-400 p-2 w-full"
-            required
-            />
+              </label>
+              <input 
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-black bg-white border border-blue-400 "
+              placeholder="Enter your username"
+              onChange={handleInputChange}
+              name="username"
+              value={username}
+              />
+             <div>
+              <label
+              htmlFor="password"
+                className="block text-sm font-semibold text-black"
+              >
+                Password
+              </label>
+              <input 
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-black bg-blue-400 "
+              placeholder="Enter your password"
+              onChange={handleInputChange}
+              value={password}
+              />
+             </div>
+             <div>
+              <label
+              htmlFor="password"
+                className="block text-sm font-semibold text-black"
+              >
+                Confirm Password
+              </label>
+              <input 
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-black bg-blue-400 "
+              placeholder="Confirm your password"
+              onChange={handleInputChange}
+              value={password}
+              />
+             </div>
           </div>
-          <div>
-            <label htmlFor="email" className="block text-black text-sm font-bold mb-2">
-              Email
-            </label>
-            <input
-            type="email"
-            name="email"
-            value={signUp.email}
-            onChange={handleChange}
-            className="border border-blue-400 p-2 w-full"
-            required
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block text-black text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-            type="password"
-            name="password"
-            value={signUp.password}
-            onChange={handleChange}
-            className="border border-blue-400 p-2 w-full"
-            required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-400 hover:bg-black hover:text-blue-400 text-black font-bold py-2 px-4 rounded"
-          >
-            Submit
-          </button>
-          <div>
-            <span>Don't have an account? <Link to={"/signup"} className="text-blue-400">Sign up</Link></span>
-          </div>
+          
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default SignUp;
