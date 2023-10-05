fc8bff3d0d4d633680f610bbee5e799b8319c236
Raymond Oeta <oetaray@gmail.com>
2023-10-05 09:56:02 +0300

    done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 //login 
 const Login = () => {
@@ -26,6 +27,9 @@ const Login = () => {
     }
   }
 
+  const handleChange = (e) => {
+    setPassword(e.target.value)
+  }  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +38,8 @@ const Login = () => {
   }
   return (
     <div className='relative flex flex-col justify-center min-h-screen overflow-hidden'>
-      <div className='w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-blue-400/60 ring ring-2 ring-gray-500 lg:max-w-xl'>
+      <div className='w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-blue-400/60 ring-2 ring-gray-500 lg:max-w-xl'>
+        <span className='mb-2 text-3xl'>SCHOLAR STUDENT MS</span>
         <h1 className='text-3xl font-semibold text-center text-black'>Log back in</h1>
         <form className='mt-6' onSubmit={handleSubmit}>
           <div className='mb-2'>
@@ -67,25 +72,25 @@ const Login = () => {
             placeholder='Enter your password'
             className='block w-full px-4 py-2 mt-2 text-black bg-white border border-blue-400 rounded-md focus:ring-blue-400 focus:ring-gray-700 focus:outline-none focus:ring focus:ring-opacity-40'
             value={password}
-            onChange={handleInputChange}
+            onChange={handleChange}
             required
             />
           </div>
-          <p className='text-xs text-blue-400 hover:text-black'>Forgotten password? Click 
-            <a href='#' className='font-medium text-blue-400 cursor-pointer hover:underline hover:text-black text-decoration-none'>here</a>
+          <p className='text-1xl black text- hover:text-black'>Forgotten password?
+            < Link to={'#'} className='font-medium text-blue-400 cursor-pointer hover:underline hover:text-black text-decoration-none' > Click here </Link>
           </p>
-          <div>
-            <button className='w-full px-4 py-2 tracking-wide text-white bg-blue-400 cursor-pointer transit hover:bg-white hover:text-blue-400' type='submit'>Log in</button>
+          <div className=''>
+            <button className='w-full px-4 py-2 tracking-wide text-white bg-blue-400 border cursor-pointer transit hover:-black hover:bg-white hover:text-blue-400 hover:border-black' type='submit'>Log in</button>
           </div>
         </form>
         <p className='mt-8 text-xs font-light text-center text-black'>
           {""}
           Don't have an account?
-          <a href='./Signup.js' className='font-medium text-blue-400 cursor-pointer hover:underline hover:text-black'>Sign up now!</a></p>
-          <p>
+          <a href='./Signup' className='font-medium text-blue-400 cursor-pointer hover:underline hover:text-black'> Sign up!</a></p>
+          <p className='text-xs'>
             {""}
             Are you an admin?
-            <a href='./admin.js' className='font-medium text-blue-400 cursor-pointer hover:underline hover:text-black'>Log in</a>
+            <a href='./admin' className='text-xs font-medium text-blue-400 cursor-pointer hover:underline hover:text-black'> Log in!</a>
           </p>
       </div>
     </div>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -21,10 +21,15 @@ function SignUp() {
     if (name === 'username'){
       setUsername(value)
     }
-    if (name === 'password'){
+    if (password === 'password'){
       setPassword(value)
     }
   }
+  
+  const handleChange = (e) => {
+    setPassword(e.target.value)
+  }
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +39,7 @@ function SignUp() {
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-blue-400/60 ring ring-2 ring-black lg-max-w-xl text-black">
+      <div className="w-full p-6 m-auto bg-white border border-black rounded-md shadow-xl shadow-blue-400/60 ring-gray-500 lg-max-w-xl">
         <h1 className="text-3xl font-semibold text-center text-black uppercase">Sign up</h1>
         <form className="mt-6" onSubmit={handleSubmit}>
           <div className="mb-2">
@@ -60,9 +65,9 @@ function SignUp() {
               </label>
               <input 
               type="password"
-              className="block w-full px-4 py-2 mt-2 text-black bg-blue-400 "
+              className="block w-full px-4 py-2 mt-2 text-black bg-white border border-blue-400 "
               placeholder="Enter your password"
-              onChange={handleInputChange}
+              onChange={handleChange}
               value={password}
               />
              </div>
@@ -75,15 +80,22 @@ function SignUp() {
               </label>
               <input 
               type="password"
-              className="block w-full px-4 py-2 mt-2 text-black bg-blue-400 "
+              className="block w-full px-4 py-2 mt-2 text-black bg-white border border-blue-400 "
               placeholder="Confirm your password"
-              onChange={handleInputChange}
+              onChange={handleChange}
               value={password}
               />
              </div>
           </div>
-          
+          <div className=''>
+            <button className='w-full px-4 py-2 tracking-wide text-white bg-blue-400 border cursor-pointer transit hover:-black hover:bg-white hover:text-blue-400 hover:border-black' type='submit'>Log in</button>
+          </div>  
         </form>
+        <p className='mt-8 text-xs font-light text-center text-black'>
+          {""}
+          Already have an account?
+          <a href='./Login' className='font-medium text-blue-400 cursor-pointer hover:underline hover:text-black'>Log back in!</a>
+        </p>
       </div>
     </div>
   );
